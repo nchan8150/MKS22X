0a1fc96761bc25a1c739c90e6bff90aed977b8b3
nchan8150 <nchan1@stuy.edu>
2018-02-12 14:48:41 -0500

    making progress!

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,27 +1,36 @@
+//JAVAC BOTH AND RUN DRIVER THEN GO FROM THERE.
+
 //size 2 and 3 wont work
 //negatives wont work
 //cant call solve twice bc its not 0 anymore
 //put 1 in each coloumn 
+//rewrite add and remove lmao
+//runtime? how slow is this really
 
 public class QueenBoard {
 
     private int[][]board;
 
     public QueenBoard (int size) {
-	new board = int[size][size];
+	board = new int[size][size];
     }
 
-    private boolean addQueen(int r, int c) {
+    public boolean addQueen(int r, int c) {
 	if (board[r][c] != 0) {
 	    return false;
 	}
-	board[r][c] = -1;
 	for(int x = r; x < board.length; x++) {
 	    board[x][c] += 1;
 	}
 	for(int y = c; y < board[r].length; y++) {
 	    board[r][y] += 1;
 	}
+	int inc = 0;
+	for(int z = r; z < board.length; z++) {
+	    board[z][c + inc] += 1;
+	    inc += 1;
+	}
+	board[r][c] = -1;
 	return true;
     }
 
@@ -29,13 +38,19 @@ public class QueenBoard {
 	if (board[r][c] != -1) {
 	    return false;
 	}
-	board[r][c] == 0;
+	
 	for(int x = r; x < board.length; x++) {
 	    board[x][c] -= 1;
 	}
 	for(int y = c; y < board[r].length; y++) {
 	    board[r][y] -= 1;
 	}
+	int inc = 0;
+	for(int z = r; z < board.length; z++) {
+	    board[z][c + inc] -= 1;
+	    inc += 1;
+	}
+	board[r][c] = 0;
 	return true;
     }
 
@@ -43,25 +58,26 @@ public class QueenBoard {
 	String bb = "";
 	for(int x = 0; x < board.length; x++) {
 	    for (int y = 0; y < board[x].length; y++) {
-		if (board[x][y] = -1) {
+		if (board[x][y] == -1) {
 		    bb += "Q ";
 		}
-		else bb += "_ ";
+		else bb += board[x][y] + " ";// "_ ";
 	    }
 	    bb += "\n";
 	}
+	return bb;
     }
 
     public boolean toSolve() {
-
+	return true;
     }
 
     public int countSolutions() {
-
+	return 0;
     }
 }
 
-
+/*
 solver(int col) {
     if(col is size) {
 	base case;
@@ -75,3 +91,4 @@ solver(int col) {
     }
     return false;
 }
+*/
