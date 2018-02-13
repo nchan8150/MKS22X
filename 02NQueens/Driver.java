9f72fe2424829415a6906472c856818e159a555c
nchan8150 <nchan1@stuy.edu>
2018-02-13 03:58:17 -0500

    only have sountSolutions to do left! woohoo

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,9 +1,13 @@
 public class Driver{
     public static void main(String[] args){
-	QueenBoard b = new QueenBoard(6);
+	QueenBoard b = new QueenBoard(2);
+
 	//	b.addQueen(2,4);
-	b.addQueen(1,1);
-	//	b.removeQueen(2,4);
+	//	b.addQueen(1,1);
+	//	b.addQueen(3,4);
+	//  	b.removeQueen(1,1);
+	System.out.println(b.solve());
 	System.out.println(b);
+	//	System.out.println(b.solve());
     }
 }
--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -15,7 +15,7 @@ public class QueenBoard {
 	board = new int[size][size];
     }
 
-    public boolean addQueen(int r, int c) {
+    private boolean addQueen(int r, int c) {
 	if (board[r][c] != 0) {
 	    return false;
 	}
@@ -25,10 +25,15 @@ public class QueenBoard {
 	for(int y = c; y < board[r].length; y++) {
 	    board[r][y] += 1;
 	}
-	int inc = 0;
-	for(int z = r; z < board.length; z++) {
-	    board[z][c + inc] += 1;
-	    inc += 1;
+	int diagDown = 0;
+	for(int z = r; z < board.length && c + diagDown < board.length; z++) {
+	    board[z][c + diagDown] += 1;
+	    diagDown += 1;
+	}
+	int diagUp = 0;
+	for(int i = r; i >= 0 && c + diagUp < board.length; i--) {
+	    board[i][c + diagUp] += 1;
+	    diagUp += 1;
 	}
 	board[r][c] = -1;
 	return true;
@@ -37,18 +42,22 @@ public class QueenBoard {
     private boolean removeQueen(int r, int c) {
 	if (board[r][c] != -1) {
 	    return false;
-	}
-	
+	}	
 	for(int x = r; x < board.length; x++) {
 	    board[x][c] -= 1;
 	}
 	for(int y = c; y < board[r].length; y++) {
 	    board[r][y] -= 1;
 	}
-	int inc = 0;
-	for(int z = r; z < board.length; z++) {
-	    board[z][c + inc] -= 1;
-	    inc += 1;
+	int diagDown = 0;
+	for(int z = r; z < board.length && c + diagDown < board.length; z++) {
+	    board[z][c + diagDown] -= 1;
+	    diagDown += 1;
+	}
+	int diagUp = 0;
+	for(int i = r; i >= 0 && c + diagUp < board.length; i--) {
+	    board[i][c + diagUp] -= 1;
+	    diagUp += 1;
 	}
 	board[r][c] = 0;
 	return true;
@@ -61,34 +70,45 @@ public class QueenBoard {
 		if (board[x][y] == -1) {
 		    bb += "Q ";
 		}
-		else bb += board[x][y] + " ";// "_ ";
+		else bb +=  "_ "; //board[x][y] + " ";
 	    }
 	    bb += "\n";
 	}
 	return bb;
     }
 
-    public boolean toSolve() {
-	return true;
+    public boolean solve() {
+	for(int x = 0; x < board.length; x++) {
+	    for(int y = 0; y < board[x].length; y++) {
+		if(board[x][y] != 0) {
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return solver(0);
     }
+    
+    public boolean solver(int col) {
+	if(col >= board.length) {
+	   return true;
+	}
+	for(int x = 0; x < board.length; x++) {
+	    if(addQueen(x, col)) {
+		if(solver(col+1)) {
+		    return true;
+		}
+		else {
+		    removeQueen(x, col);
+		}
+	    }
+	}
+	return false;
+    }
+
 
     public int countSolutions() {
 	return 0;
     }
 }
 
-/*
-solver(int col) {
-    if(col is size) {
-	base case;
-    }
-    for(each row) {
-	place a queen;
-	if (solver(col+1)) {
-	    return true;
-	}
-	else { removeQueen;}
-    }
-    return false;
-}
-*/
+
