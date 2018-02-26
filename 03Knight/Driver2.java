165d35c97e9942caa2e53c6f8b6dae1ea0120529
nchan8150 <nchan1@stuy.edu>
2018-02-26 00:14:46 -0500

    toString works probably

---
+++ 03Knight/Driver2.java
@@ -0,0 +1,6 @@
+public class Driver2 {
+    public static void main(String[] args) {
+	KnightBoard b = new KnightBoard(2, 5);
+	System.out.println(b.toString());
+    }
+}
--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,25 +1,44 @@
-public KnightBoard(int startingRows, int startingCols) {
+public class KnightBoard{
     private int [][] board;
 
     public KnightBoard(int startingRows, int startingCols) {
 	board = new int[startingRows][startingCols];
+
     }
 
     public String toString() {
-
+	String ans = "";
+	for(int x = 0; x < board.length; x++) {
+	    for(int y = 0; y < board[x].length; y++){
+		if(board[x][y] == 0){
+		    ans+= " _ ";
+		}
+		else {
+		    if(board[x][y] < 10) {
+			ans += " "+board[x][y]+" ";
+		    }
+		    else {
+			ans += board[x][y] + " ";
+		    }
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
     }
-
+    /*
     public boolean solve(int startingRow, int startingCol) {
-
+	return true;
     }
 
     public boolean countSolutions(int startingRow, int startingCol) {
-
+	return true;
     }
 
     private boolean solveH(int row, int col, int level) {
-
+	return true;
     }
+    */
     
 }
     
