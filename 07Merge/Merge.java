c8ff515e633101c9725a7bae01e7749240ce8f8c
nchan8150 <nchan1@stuy.edu>
2018-03-20 14:50:15 -0400

    progress on 07!

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,3 +1,16 @@
 public class Merge {
 
+    public void mergesort(int[] data) {
+	int[] temp = new int[data.length];
+	msort(data, temp, 0, data.length - 1);
+    }
+
+    public void msort(int[]data, int[]temp, int lo, int hi) {
+	msort(temp, data, lo, (lo+hi) / 2);
+	msort(temp, data,
+    }
+
+    public void merge(int[]data) {
+
+    }
 }
