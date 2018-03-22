d75fc9313273417f8e9d36ef321f8d9ecc6bab3f
nchan8150 <nchan1@stuy.edu>
2018-03-22 14:40:10 -0400

    adding on to 07

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -7,7 +7,8 @@ public class Merge {
 
     public void msort(int[]data, int[]temp, int lo, int hi) {
 	msort(temp, data, lo, (lo+hi) / 2);
-	msort(temp, data,
+	msort(temp, data, (lo+hi) / 2, hi);
+	merge(data, temp, lo, hi);
     }
 
     public void merge(int[]data) {
