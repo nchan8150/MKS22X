ba233cef5612335c0a4c1cca49ff926374c04448
nchan8150 <nchan1@stuy.edu>
2018-03-26 01:38:20 -0400

    Still need to test merge

---
+++ 06QuickSort/Quick.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Quick{
+    public static int partition (int[] data, int start, int end) {
+	Random rand = new Random();
+	int indexPivot = rand(end - start + 1) + start;
+
+	int pivot = data[randPivot];
+	
+        swap(data, 0, indexPivot);
+	while( end > start) {
+	    if(data[start] < pivot) {
+		start++;
+	    }
+	    else {
+		swap(data, start, end);
+		end--;
+	    }
+	}
+
+	swap(data, 0, end);
+	return end;
+    }
+
+    public static int quickselect(int[] data, int k) {
+	if(data[k] < pivot) {
+	    partition(0,pivot)
+    }
+
+    public static void swap (int[] ary, int a, int b) {
+	int c = ary[a];
+	ary[a] = ary[b];
+	ary[b] = c;
+    }  
+    
+}
--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -8,17 +8,52 @@ public class Merge {
     }
 
     public void msort(int[]data, int[]temp, int lo, int hi) {
-	if(lo < hi) {
-	    for(int x = lo; x <= hi; x++) {
-		temp[x] = data[x];
-	    }
+	if(hi - lo < 2) {
+	    insertionSort(temp);
+	}
 	msort(temp, data, lo, (lo+hi) / 2);
 	msort(temp, data, (lo+hi) / 2, hi);
 	merge(data, temp, lo, hi);
 	}
+
+    public void merge(int[]data, int[]temp, int lo, int hi) {
+	int first = lo;
+	int next = ((lo + hi) / 2) + 1;
+	for (int x = lo; x <= hi; x++) {
+	    if(first > (lo + hi) / 2) {
+		temp[x] = data[next];
+		next++;
+	    }
+	    else if (next <= hi) {
+		if(data[first] < data[next]) {
+		    temp[x] = data[first];
+		    first++;
+		}
+		else{
+		    temp[x] = data[first];
+		    next++;			 
+		}
+	    }
+	    else{
+		temp[x] = data[first];
+		first++;
+	    }
+	}
+	for (int y = lo; y < hi; y++) {
+	    data[y] = temp[y];
+	}
+	    
     }
 
-    public void merge(int[]data) {
-
+    public void insertionSort(int[] data) {
+	for(int i = 0; i < data.length; i++) {
+	    int key = data[i];
+	    int j = i - 1;
+	    while( j >= 0 && data[j] > key) {
+		data[j + 1] = data[j];
+		j = j- 1;
+	    }
+	    data[j+1] = key;
+	}
     }
 }
