2e649b691c60ada0a2f3cae9c9cbe88a8d7a0639
nchan8150 <nchan1@stuy.edu>
2018-03-26 02:41:30 -0400

    working on 06

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,36 +1,63 @@
 import java.util.*;
 
-public class QuickSelect {
-    public static int partition (int[] data, int start, int end) {
-	Random rand = new Random();
-	int indexPivot = rand(end - start + 1) + start;
-
-	int pivot = data[randPivot];
-	
-        swap(data, 0, indexPivot);
-	while( end > start) {
-	    if(data[start] < pivot) {
-		start++;
-	    }
-	    else {
-		swap(data, start, end);
-		end--;
-	    }
-	}
-
-	swap(data, 0, end);
-	return end;
-    }
+public class Quick{
 
     public static int quickselect(int[] data, int k) {
 	if(data[k] < pivot) {
-	    partition(0,pivot)
+	    partition(0,pivot);
+	}
+    }
+	    
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high]; 
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++){
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot){
+                i++;
+ 
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+ 
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+ 
+        return i+1;
+    }
+ 
+
+    void sort(int[] arr, int low, int high){
+        if (low < high){
+            /* pi is partitioning index, arr[pi] is 
+              now at right place */
+            int pi = partition(arr, low, high);
+ 
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
     }
 
-    public static void swap (int[] ary, int a, int b) {
-	int c = ary[a];
-	ary[a] = ary[b];
-	ary[b] = c;
-    }  
+ 
+    // Driver program
+    public static void main(String[] args) {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+ 
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n-1);
+ 
+        System.out.println("sorted array");
+        printArray(arr);
+    }
+    
     
 }
