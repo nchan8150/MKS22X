d8e2acb00f7e7d077d498006ecb8e783eec420bd
nchan8150 <nchan1@stuy.edu>
2018-03-28 14:41:19 -0400

    skeleton for 08

---
+++ 08LinkedList/MyLinkedList.java
@@ -0,0 +1,54 @@
+public class LinkedList {
+    private Node first, last;
+    private int length;
+
+    public String toString() {
+
+    }
+
+    public int get(int index) {
+	
+    }
+
+    public void set(int index, int value) {
+
+    }
+
+    public int size() {
+	return length;
+    }
+
+    public boolean add(int index, int value) {
+
+    }
+
+    public boolean remove(int index) {
+
+    }
+    
+    private class Node {
+	private Node next, prev;
+	private int data;
+
+	public Node getNext() {
+
+	}
+
+	public Node getPrev() {
+
+	}
+
+	public int getValue() {
+
+	}
+
+	public int set(int index) {
+
+	}
+
+	public String toString() {
+
+	}	    
+    }
+}
+    
