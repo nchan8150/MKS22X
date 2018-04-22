b2bc671c9c880d91b516287465b072746e95136d
nchan8150 <nchan8150@LAPTOP-OBFS30ET.localdomain>
2018-04-22 01:30:58 -0400

    Weird test :/

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -6,7 +6,7 @@ public class Driver{
     public static void main(String[]args){
 	try {
 	Maze f;
-        f = new Maze("data4.dat");//true animates the maze.
+        f = new Maze("data3.dat");//true animates the maze.
         
         f.setAnimate(true);
         f.solve();
