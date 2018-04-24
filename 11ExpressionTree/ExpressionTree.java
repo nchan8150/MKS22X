05c4f18388112a32565ecc5fe21ed52dbf53c10e
nchan8150 <nchan1@stuy.edu>
2018-04-24 14:49:18 -0400

    working on 11

--- 11ExpressionTree/ExpressionTree.java
+++ 11ExpressionTree/ExpressionTree.java
@@ -43,9 +43,21 @@ public class ExpressionTree{
     
     /*use the correct operator on both a and b, and return that value*/
     private double apply(char op, double a, double b){
-	/*you are to write this method*/
-	return 0.0;
-	
+	if (op.equals('+')) {
+	    return a + b;
+	}
+	if (op.equals('-')) {
+	    return a - b;
+	}
+	if (op.equals('*')) {
+	    return a * b;
+	}
+	if (op.equals('/')) {
+	    return a / b;
+	}
+	if (op.equals('%')) {
+	    return a % b;
+	}	
     }
 
     
