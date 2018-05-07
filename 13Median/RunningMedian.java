1068d13636690d98eec9f3b0a6f63491e498d827
nchan8150 <nchan8150@LAPTOP-OBFS30ET.localdomain>
2018-05-07 05:41:36 -0400

    working on 13

--- 13Median/RunningMedian.java
+++ 13Median/RunningMedian.java
@@ -1,1 +1,24 @@
-h
+public class RunningMedian{
+
+	private int size;
+
+
+	public RunningMedian() {
+		size = 0;
+	}
+
+	public void add (Double added) {
+
+	}
+
+	public Double getMedian() {
+		if(size == 0) {
+			throw new NoSuchElementException();
+		}
+
+	}
+
+	public int size() {
+		return size;
+	}	
+}
