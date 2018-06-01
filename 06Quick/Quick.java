2cd5cfa249cc526925582074a8ce22bbd0dbef0c
nchan8150 <nchan1@stuy.edu>
2018-06-01 00:27:05 -0400

    Fixed 06 I think...

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -23,6 +23,17 @@ public class Quick{
 	}
 	return data[low];
     }
+
+    public static void insertionsort(int[] data, int lo, int hi){ 
+        int ind;
+        for (int i = lo; i < hi+1; i ++) {
+            int k = data[i];
+            for (ind = i; ind > lo && k < data[ind-1]; ind --) {
+                data[ind] = data[ind-1];
+            }
+            data[ind] = k;
+        }    
+    }
 	    
     public static int[] partition(int[] arr, int low, int high){
         int pivotIndex = (int)(Math.random() * (high - low) + low);
@@ -53,17 +64,43 @@ public class Quick{
     }
 
     public static void quicksorter (int[] data, int start, int end) {
-	if (start < end) {
-	    int[] temp = partition(data, start, end);
-	    quicksorter(data, start, temp[0]);
-	    quicksorter(data, temp[1], end);
+		if (end - start <= 2){
+	    	insertionsort(data, start, end);
+		}
+		else{ int[] pivot = partitionD(data, start, end);
+            quicksorter(data, start, pivot[0]);
+           	quicksorter(data, pivot[1], end);
+        }
 	}
-    }
-    
+
     private static void swap(int[]arr, int x, int y) {
 	int i = arr[x];
 	arr[x] = arr[y];
 	arr[y] = i;
+    }
+
+    public static int[] partitionD(int[]data, int low, int hi){
+        int pivot = (int) (Math.random() * (hi - low)) + low; 
+        int pivoter = data[pivot];
+        swap (data,low,pivot);
+        int lt = low;
+        int gt = hi;
+        int i = lt + 1;
+        while(i <= gt){
+            if (data[i] < pivoter){
+                swap(data,i,lt);
+                i ++;
+                lt++;
+            }
+            else if (data[i] == pivoter){
+                i++;
+            }
+            else if (data[i] > pivoter){
+                swap(data,i,gt);
+                gt--;
+            }
+        }
+        return new int[] {lt, i};
     }
     
     
